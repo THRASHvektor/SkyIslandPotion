f74b743ff513c40c3cdfc729db96009e49fe6eca
Cai Shiteng <1132357884@qq.com>
2026-02-09 22:47:08 +0800

    Initial Project

---
+++ Client/SIP/Source/SIP/GameMode/SIPGameMode.h
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SIPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASIPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASIPGameMode();
+
+	//virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+};
+
+
+
