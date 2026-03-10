264ce6df6f7c1e4dddc5a1373c61bacac627da74
Cai Shiteng <1132357884@qq.com>
2026-03-10 23:48:30 +0800

    可交互物体Interact函数添加蓝图实现接口 方便后续子类在蓝图中书写交互逻辑

--- Client/SIP/Source/SIP/Character/Interactable.h
+++ Client/SIP/Source/SIP/Character/Interactable.h
@@ -27,17 +27,24 @@ class SIP_API IInteractable
     GENERATED_BODY()
 
 public:
-    // 进行交互，供可交互对象实现交互结果，一般由character的交互组件调用
-    virtual void Interact(UAbilitySystemComponent* InteractorASC) = 0;
-
     // 获取交互提示文字（例如："采集药草、对话"）
     virtual FText GetInteractText() const = 0;
 
+    /**
+     * 如果后期需要由蓝图决定是否执行cpp，可将其合并成一个BlueprintNativeEvent
+     */
+    // 进行交互，供可交互对象实现交互结果，此函数提供蓝图实现接口，可用来实现一些游戏逻辑
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Interaction")
+    void OnInteract_BP(UAbilitySystemComponent* InteractorASC);
+
+    // 进行交互，供可交互对象实现交互结果，一般由character的交互组件调用，一些流程性代码必须在cpp中实现
+    virtual void Interact(UAbilitySystemComponent* InteractorASC) { Execute_OnInteract_BP(_getUObject(), InteractorASC); };
+
+
     // 当玩家看向/靠近物体，且该物体被选为“最佳目标”时调用
-    // UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
     virtual void OnBeginFocus() = 0;
 
+
     // 当玩家视线移开或离开范围时调用
-    // UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
     virtual void OnEndFocus() = 0;
 };
