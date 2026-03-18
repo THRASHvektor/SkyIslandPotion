3810a14ee789d0e7b28134bdbc72e5e7fc3b044e
qmm770322 <152303808+qmm770322@users.noreply.github.com>
2026-03-19 02:32:47 +0800

    宠物跟随行为树

--- Client/SIP/Source/SIP/Character/SIPPetCharacter.h
+++ Client/SIP/Source/SIP/Character/SIPPetCharacter.h
@@ -1,40 +1,25 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include "AbilitySystemInterface.h"
+#include "Character/SIPCharacter.h" 
 #include "SIPPetCharacter.generated.h"
-class USIPAbilitySystemComponent;
-class UAttributeSet;
+
+UENUM(BlueprintType)
+enum class EPetState : uint8
+{
+	Wild		UMETA(DisplayName = "Wild"),
+	Companion	UMETA(DisplayName = "Companion"),
+	Worker		UMETA(DisplayName = "Worker")
+};
+
 UCLASS()
-class SIP_API ASIPPetCharacter : public ACharacter, public IAbilitySystemInterface
+class SIP_API ASIPPetCharacter : public ASIPCharacter
 {
 	GENERATED_BODY()
-
-public:
-	// Sets default values for this character's properties
-	ASIPPetCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
-
-protected:
-	// 安装GAS
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Abilities", meta = (AllowPrivateAccess = "true"))
-	USIPAbilitySystemComponent* AbilitySystemComponent;
-
-	// 安装属性集
-	UPROPERTY()
-	const UAttributeSet* AttributeSet;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pet Data")
-    class USIPPetDataAsset* PetData;
+public:
+	ASIPPetCharacter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pet AI")
+	EPetState CurrentPetState = EPetState::Wild; 
 };
